d3b2546c193810df68a8b8b0bbf78414f1f21d33
Goga Token <gogibugo@gmail.com>
2024-05-31 20:01:34 +0500

    add getTxHistorye() method

--- ScriptPubKey.go
+++ ScriptPubKey.go
@@ -7,15 +7,15 @@ import (
 	"github.com/btcsuite/btcutil/bech32"
 )
 
-func getScriptPubKey(address string) ([]byte, error) {
+func getScriptPubKey(address string) (string, error) {
 	if strings.HasPrefix(address, "bc1") {
 		_, data, err := bech32.Decode(address)
 		if err != nil {
-			return nil, err
+			return "", err
 		}
 		converted, err := bech32.ConvertBits(data[1:], 5, 8, false)
 		if err != nil {
-			return nil, err
+			return "", err
 		}
 		var scriptPubKey []byte
 		if data[0] == 0x00 { // P2WPKH
@@ -23,28 +23,33 @@ func getScriptPubKey(address string) ([]byte, error) {
 		} else { // Other SegWit versions
 			scriptPubKey = append([]byte{data[0], byte(len(converted))}, converted...)
 		}
-		return scriptPubKey, nil
+		result := fmt.Sprintf("%x", scriptPubKey)
+		return result, nil
 	} else if strings.HasPrefix(address, "3") {
 		decodedAddress := DecodeBase58(address)
 		if len(decodedAddress) != 25 {
-			return nil, fmt.Errorf("invalid address length")
+			return "", fmt.Errorf("invalid address length")
 		}
 		scriptHash := decodedAddress[1:21]
 		scriptPubKey := append([]byte{0xa9, 0x14}, scriptHash...)
 		scriptPubKey = append(scriptPubKey, 0x87)
-		return scriptPubKey, nil
+		result := fmt.Sprintf("%x", scriptPubKey)
+
+		return result, nil
 	} else if strings.HasPrefix(address, "1") {
 		decodedAddress := DecodeBase58(address)
 		if len(decodedAddress) != 25 {
-			return nil, fmt.Errorf("invalid address length")
+			return "", fmt.Errorf("invalid address length")
 		}
 		pubKeyHash := decodedAddress[1:21]
 		scriptPubKey := append([]byte{0x76, 0xa9, 0x14}, pubKeyHash...)
 		scriptPubKey = append(scriptPubKey, 0x88, 0xac)
-		return scriptPubKey, nil
+		result := fmt.Sprintf("%x", scriptPubKey)
+		return result, nil
 	} else {
-		return nil, fmt.Errorf("unsupported address type")
+		return "", fmt.Errorf("unsupported address type")
 	}
+
 }
 
 func DecodeBase58(b string) []byte {
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/crytoken/electrum-go
 
 go 1.22.2
+
+require github.com/btcsuite/btcutil v1.0.2
